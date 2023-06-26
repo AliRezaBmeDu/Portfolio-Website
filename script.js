268d1697342482b3fcbba3baa551e12b7af984fc
AliReza <ali.reza.buet@gmail.com>
2023-06-27 00:28:03 +0600

    Resolve the linter errors

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 const menuButton = document.querySelector('.button-burger');
-const excepSection = document.querySelector(".myInfo");
-const closeButton = document.querySelector(".close-button");
+const excepSection = document.querySelector('.myInfo');
+const closeButton = document.querySelector('.close-button');
 const otherComponents = document.querySelectorAll('section');
-const menuPage = document.querySelector(".mobile-menu");
+const menuPage = document.querySelector('.mobile-menu');
+const navButtons = document.querySelectorAll('.menu-list li a');
 
 menuButton.addEventListener('click', () => {
-    console.log("Hello, it's being called");
-    otherComponents.forEach(subelements => {
-        subelements.classList.add("hide-seek");
-      });
-    excepSection.classList.remove("hide-seek");
-    menuPage.style.display= 'block';
-    menuButton.style.display = 'none';
-    excepSection.classList.add('blur-effect');
+  console.log("Hello, it's being called");
+  otherComponents.forEach((subelements) => {
+    subelements.classList.add('hide-seek');
+  });
+  excepSection.classList.remove('hide-seek');
+  menuPage.style.display = 'block';
+  menuButton.style.display = 'none';
+  excepSection.classList.add('blur-effect');
 });
-
 
 closeButton.addEventListener('click', () => {
-    console.log("Hello, closebutton is being called");
-    menuPage.style.display = "none";
-    menuButton.style.display = 'block';
-    excepSection.classList.remove('blur-effect');
-    otherComponents.forEach(subelements => {
-        subelements.classList.remove("hide-seek");
-      });
-});
-
-
-function closeMenu() {
-  console.log("Hello, closebutton is being called");
-  menuPage.style.display = "none";
+  console.log('Hello, closebutton is being called');
+  menuPage.style.display = 'none';
   menuButton.style.display = 'block';
   excepSection.classList.remove('blur-effect');
-  otherComponents.forEach(subelements => {
-      subelements.classList.remove("hide-seek");
+  otherComponents.forEach((subelements) => {
+    subelements.classList.remove('hide-seek');
+  });
+});
+
+navButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log('Hello, closebutton is being called');
+    menuPage.style.display = 'none';
+    menuButton.style.display = 'block';
+    excepSection.classList.remove('blur-effect');
+    otherComponents.forEach((subelements) => {
+      subelements.classList.remove('hide-seek');
     });
-}
+  });
+});
