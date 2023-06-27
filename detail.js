c60b71bb73fc717e06265309038b46eabd937e82
AliReza <ali.reza.buet@gmail.com>
2023-06-27 09:46:55 +0600

    Create the project detail database

---
+++ detail.js
@@ -0,0 +1,54 @@
+const proj_db = [
+    {
+        titleMobile: "Tonic",
+        overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgMobile: "./works/project01.png",
+        builtwithMobile: ["HTML", "CSS", "javaScript"],
+        titleDesktop: "Tonic",
+        descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgDesktop: "./images/desktop/project01.png",
+        builtwithDesktop: ["HTML","CSS","javaScript"],
+        
+    },
+    {
+        titleMobile: "Multi-Post Stories",
+        overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgMobile: "./works/project02.png",
+        builtwithMobile: ["HTML", "CSS", "javaScript"],
+        titleDesktop: "Multi-Post Stories",
+        overheadDesktop: ["FACEBOOK", "Full Stack Dev", "2015"],
+        descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgDesktop: "./images/desktop/project02.png",
+        builtwithDesktop: ["HTML","Ruby on rails","CSS","javaScript"],
+        
+    },
+    {
+        titleMobile: "Tonic",
+        overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgMobile: "./works/project03.png",
+        builtwithMobile: ["HTML", "CSS", "javaScript"],
+        titleDesktop: "FACEBOOK360",
+        overheadDesktop: ["FACEBOOK", "Full Stack Dev", "2015"],
+        descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgDesktop: "./images/desktop/project03.png",
+        builtwithDesktop: ["HTML","Ruby on rails","CSS","javaScript"],
+        
+    },
+    {
+        titleMobile: "Multi-Post Stories",
+        overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgMobile: "./works/project04.png",
+        builtwithMobile: ["HTML", "CSS", "javaScript"],
+        titleDesktop: "Uber Navigation",
+        overheadDesktop: ["Uber", "Lead Developer", "2018"],
+        descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        imgDesktop: "./images/desktop/project04.png",
+        builtwithDesktop: ["HTML","Ruby on rails","CSS","javaScript"],
+        
+    }
+    
+]
