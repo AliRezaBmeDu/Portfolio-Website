14baee252f6f5d14963accb195a64c7f51758a39
AliReza <ali.reza.buet@gmail.com>
2023-06-28 10:01:40 +0600

    Working on dynamic work-section

--- detail.js
+++ detail.js
@@ -3,12 +3,15 @@ const proj_db = [
         titleMobile: "Tonic",
         closeButtonIcon: "./icons/closeButtonIcon.png",
         overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        shortdescMobile: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
         descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgMobile: "./works/project01.png",
         builtwithMobile: ["HTML", "CSS", "javaScript"],
         liveIcon: "./icons/liveIcon.png",
         sourceIcon: "./icons/sourceIcon.png",
         titleDesktop: "Tonic",
+        overheadDesktop: ["Canopy", "Back End Dev", "2015"],
+        shortdescDesktop: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
         descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgDesktop: "./images/desktop/project01.png",
         builtwithDesktop: ["HTML","CSS","javaScript"],
@@ -18,6 +21,7 @@ const proj_db = [
         titleMobile: "Multi-Post Stories",
         closeButtonIcon: "./icons/closeButtonIcon.png",
         overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        shortdescMobile: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
         descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgMobile: "./works/project02.png",
         builtwithMobile: ["HTML", "CSS", "javaScript"],
@@ -25,6 +29,7 @@ const proj_db = [
         sourceIcon: "./icons/sourceIcon.png",
         titleDesktop: "Multi-Post Stories",
         overheadDesktop: ["FACEBOOK", "Full Stack Dev", "2015"],
+        shortdescDesktop: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
         descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgDesktop: "./images/desktop/project02.png",
         builtwithDesktop: ["HTML","Ruby on rails","CSS","javaScript"],
@@ -34,6 +39,7 @@ const proj_db = [
         titleMobile: "Tonic",
         closeButtonIcon: "./icons/closeButtonIcon.png",
         overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        shortdescMobile: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
         descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgMobile: "./works/project03.png",
         builtwithMobile: ["HTML", "CSS", "javaScript"],
@@ -41,6 +47,7 @@ const proj_db = [
         sourceIcon: "./icons/sourceIcon.png",
         titleDesktop: "FACEBOOK360",
         overheadDesktop: ["FACEBOOK", "Full Stack Dev", "2015"],
+        shortdescDesktop: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
         descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgDesktop: "./images/desktop/project03.png",
         builtwithDesktop: ["HTML","Ruby on rails","CSS","javaScript"],
@@ -50,6 +57,7 @@ const proj_db = [
         titleMobile: "Multi-Post Stories",
         closeButtonIcon: "./icons/closeButtonIcon.png",
         overheadMobile: ["Canopy", "Back End Dev", "2015"],
+        shortdescMobile: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
         descMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgMobile: "./works/project04.png",
         builtwithMobile: ["HTML", "CSS", "javaScript"],
@@ -57,6 +65,7 @@ const proj_db = [
         sourceIcon: "./icons/sourceIcon.png",
         titleDesktop: "Uber Navigation",
         overheadDesktop: ["Uber", "Lead Developer", "2018"],
+        shortdescDesktop: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
         descDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
         imgDesktop: "./images/desktop/project04.png",
         builtwithDesktop: ["HTML","Ruby on rails","CSS","javaScript"],
@@ -67,6 +76,149 @@ const proj_db = [
 const buttonID = {
     see1: 0, see2: 1, see3: 2, see4: 3
 }
+
+//........Building the Work Section........//
+
+const workContainer = document.getElementById("works");
+for(i=0; i<4; i++){
+    const singleProject = document.createElement('div');
+    singleProject.classList.add("project-card");
+    singleProject.id = `${i+1}-project`;
+    //img Desktop
+    const imgWrapDtop = document.createElement('a')
+    imgWrapDtop.href = "";
+    imgWrapDtop.classList.add('project-img','desktop');
+    const imgDtop = document.createElement('img');
+    imgDtop.src = proj_db[i].imgDesktop;
+    imgWrapDtop.appendChild(imgDtop);
+    //img Mobile
+    const imgWrapMobile = document.createElement('a')
+    imgWrapMobile.href = "";
+    imgWrapMobile.classList.add('project-img','mobile');
+    const imgMobile = document.createElement('img');
+    imgMobile.src = proj_db[i].imgMobile;
+    imgWrapMobile.appendChild(imgMobile);
+    //proj-desktop:: first key letter-d
+    const dProjDtop = document.createElement('div'); //81no.line
+    //first title
+    const mFirstTitle = document.createElement('h1');
+    mFirstTitle.textContent = proj_db[i].titleMobile;
+    mFirstTitle.classList.add('mobile')
+    mFirstTitle.classList.add('title1');
+    const dFirstTitle = document.createElement('h1');
+    dFirstTitle.textContent = proj_db[i].titleDesktop;
+    dFirstTitle.classList.add(`title${i+1}`,'desktop');
+    //proj-overhead
+    const dProjOverhead = document.createElement('div');
+    dProjOverhead.classList.add('project-overhead');
+    const dOverhead =document.createElement('ul');
+    dOverhead.classList.add('overhead');
+    if(i==0){
+        dOverhead.classList.add('new');
+    }
+    //mobile
+        const mLiElem1 = document.createElement('li');
+        mLiElem1.classList.add('overhead-1','mobile');
+        mLiElem1.textContent = proj_db[i].overheadMobile[0];
+        const mLiElem2 = document.createElement('li');
+        mLiElem2.textContent = proj_db[i].overheadMobile[1];
+        mLiElem2.classList.add('mobile');
+        const mLiElem3 = document.createElement('li');
+        mLiElem3.textContent = proj_db[i].overheadMobile[2];
+        mLiElem3.classList.add('mobile');
+        dOverhead.appendChild(mLiElem1);
+        dOverhead.appendChild(mLiElem2);
+        dOverhead.appendChild(mLiElem3);
+    //desktopOverheadlist
+    const dLiElem1 = document.createElement('li');
+    dLiElem1.classList.add('overhead-1');
+    dLiElem1.classList.add('desktop');
+    dLiElem1.textContent = proj_db[i].overheadDesktop[0];
+    const dLiElem2 = document.createElement('li');
+    dLiElem2.textContent = proj_db[i].overheadDesktop[1];
+    dLiElem2.classList.add('desktop');
+    const dLiElem3 = document.createElement('li');
+    dLiElem3.textContent = proj_db[i].overheadDesktop[2];
+    dLiElem3.classList.add('desktop');
+    dOverhead.appendChild(dLiElem1);
+    dOverhead.appendChild(dLiElem2);
+    dOverhead.appendChild(dLiElem3);
+    dProjOverhead.appendChild(dOverhead);
+    //description part
+    const mProjectDetail = document.createElement('p'); //90no.line
+    mProjectDetail.classList.add('project-detail','mobile');
+    mProjectDetail.textContent = "A daily selection of privately personalized reads; no accounts or sign-ups required.";
+
+    const dProjectDetail = document.createElement('p'); //90no.line
+    dProjectDetail.classList.add('project-detail','desktop');
+    dProjectDetail.textContent = proj_db[i].shortdescDesktop[i];
+
+    const dBuiltWith = document.createElement('ul');
+    dBuiltWith.classList.add('built-with');
+    if(i==0){
+        dBuiltWith.classList.add('newbltwth');
+    }
+    //list of tech
+    const dLiTech1 = document.createElement('li');
+    const dLiButton1 = document.createElement('button');
+    dLiButton1.classList.add('build');
+    dLiButton1.textContent = 'HTML';
+    dLiTech1.appendChild(dLiButton1);
+    dBuiltWith.appendChild(dLiTech1);
+
+    if(i==2 || i==3){
+        const dLiTech4 = document.createElement('li');
+        dLiTech4.classList.add('desktop');
+        const dLiButton4 = document.createElement('button');
+        dLiButton4.classList.add('build','desktop');
+        dLiButton4.textContent = 'Ruby on rails';
+        dLiTech4.appendChild(dLiButton4);
+        dBuiltWith.appendChild(dLiTech4);
+    }
+
+    const dLiTech2 = document.createElement('li');
+    const dLiButton2 = document.createElement('button');
+    dLiButton2.classList.add('build');
+    dLiButton2.textContent = 'CSS';
+    dLiTech2.appendChild(dLiButton2);
+    dBuiltWith.appendChild(dLiTech2);
+
+    const dLiTech3 = document.createElement('li');
+    const dLiButton3 = document.createElement('button');
+    dLiButton3.classList.add('build');
+    dLiButton3.textContent = 'javaScript';
+    dLiTech2.appendChild(dLiButton3);
+    dBuiltWith.appendChild(dLiTech3);
+
+
+    const dSeeButton = document.createElement('button');
+    dSeeButton.classList.add('see');
+    dSeeButton.textContent = "See Project";
+
+    dProjDtop.appendChild(dFirstTitle);
+    dProjDtop.appendChild(mFirstTitle);
+    dProjDtop.appendChild(dProjOverhead);
+    dProjDtop.appendChild(mProjectDetail);
+    dProjDtop.appendChild(dProjectDetail);
+    dProjDtop.appendChild(dBuiltWith);
+    dProjDtop.appendChild(dSeeButton);
+
+    //changeblock
+    if(i==0 || i==2){
+        singleProject.appendChild(imgWrapDtop)
+        singleProject.appendChild(imgWrapMobile)
+        singleProject.appendChild(dProjDtop)
+    }else{
+        singleProject.appendChild(imgWrapMobile)
+        singleProject.appendChild(dProjDtop)
+        singleProject.appendChild(imgWrapDtop)
+        }
+    
+    workContainer.appendChild(singleProject);
+}
+
+
+
 //--------Building Project Card for Mobile Version-----//
 for(let i=0; i<4; i++){
     const projectCard = document.createElement("div"); //main card
