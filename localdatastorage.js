393b2c86788ed3b94af0538e6aea70ab29a02dab
AliReza <ali.reza.buet@gmail.com>
2023-06-30 09:06:59 +0600

    Create the localstorage

---
+++ localdatastorage.js
@@ -0,0 +1,29 @@
+const fullname = document.querySelector('#name');
+const email = document.querySelector('#email');
+const comment = document.querySelector('#comment');
+
+function updateStorage() {
+  localStorage.setItem(
+    'userDetails',
+    JSON.stringify({
+        name: fullname.value,
+        email: email.value,
+        message: comment.value,
+    }),
+  );
+}
+
+fullname.oninput = updateStorage;
+email.oninput = updateStorage;
+comment.oninput = updateStorage;
+
+// function populateInput() {
+//   const retrievedItem = JSON.parse(localStorage.getItem('userDetails'));
+//   fullname.value = retrievedItem.name;
+//   email.value = retrievedItem.email;
+//   comment.value = retrievedItem.message;
+// }
+
+// if (localStorage.getItem('userDetails')) {
+//   populateInput();
+// }
